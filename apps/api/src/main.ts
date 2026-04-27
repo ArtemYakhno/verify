0df93c723b217d33b5ed701957f277a16c851d91
Artem Yakhno <artem.yakhno.ua@gmail.com>
2026-04-27 19:24:22 +0300

    init: setup back and front

---
+++ apps/api/src/main.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Verify API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
+
+// Backend works on http://localhost:3000
+// Swagger: http://localhost:3000/api/docs;
